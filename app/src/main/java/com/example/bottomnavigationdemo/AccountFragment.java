70628e62322897abe4958a36c432db90721e76ea
leslie <744632574@qq.com>
2021-04-14 22:34:51 +0800

    add logout button

--- app/src/main/java/com/example/bottomnavigationdemo/AccountFragment.java
+++ app/src/main/java/com/example/bottomnavigationdemo/AccountFragment.java
@@ -2,34 +2,54 @@ package com.example.bottomnavigationdemo;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.bottomnavigationdemo.databinding.AccoountFragmentBinding;
+import com.example.bottomnavigationdemo.databinding.ContactFragmentBinding;
+
+import cn.leancloud.AVUser;
+
 public class AccountFragment extends Fragment {
 
-    private AccountViewModel mViewModel;
+    private UserViewModel mViewModel;
+    private AccoountFragmentBinding binding;
 
     public static AccountFragment newInstance() {
         return new AccountFragment();
     }
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.accoount_fragment, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = AccoountFragmentBinding.inflate(inflater, container, false);
+
+        binding.logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mViewModel.logout();
+                Intent intent = new Intent(getActivity(),  MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        return binding.getRoot();
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(AccountViewModel.class);
+        mViewModel = new ViewModelProvider(this).get(UserViewModel.class);
         // TODO: Use the ViewModel
     }
 
